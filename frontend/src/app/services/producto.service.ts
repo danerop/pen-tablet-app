9a6b8f6eadb2da5c645f5da6e34b9f6f6171812a
Manuel Oyaregui <manu.oyaregui@gmail.com>
2022-10-27 12:34:08 -0300

    refactorizo el getById

--- frontend/src/app/services/producto.service.ts
+++ frontend/src/app/services/producto.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { productosDeEjemplo } from '../data';
 import { Producto } from '../models/producto';
 
@@ -15,19 +16,7 @@ export class ProductoService {
     return productosDeEjemplo;
   }
 
-  getById(idParam:number):Producto{
-    let productoObtenido: Producto = new Producto;
-    const httpGet_ = this._http.get<Producto>('/api/prodId/'+ idParam);
-    
-    httpGet_.subscribe( (data:Producto) => {
-      productoObtenido.id = data.id;
-      productoObtenido.nombre = data.nombre;
-      productoObtenido.clasificacion = data.clasificacion;
-      productoObtenido.descripcion = data.descripcion;
-      productoObtenido.imgUrl = data.imgUrl;
-      productoObtenido.precio = data.precio;
-    });
-
-    return productoObtenido;
+  getById(idParam:number):Observable<Producto>{
+    return this._http.get<Producto>('/api/prodId/'+ idParam);
   }
 }
