7c89c28ec13ec301e5f4101973387ffc286bd7f0
Emanuel Gomez <48289234+Emakin@users.noreply.github.com>
2022-11-10 20:17:48 -0300

    agrego mis cambios

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'Manumanu08',
+    password: 'admin',
     database: 'pentablet',
     port: 3306
 });
