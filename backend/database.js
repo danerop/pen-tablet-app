6bb04c93efa275c17959f3cf9cd2c08e5441a4dc
Manuel Oyaregui <manu.oyaregui@gmail.com>
2022-11-11 15:05:10 -0300

    agrego un par de estilos

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'admin',
+    password: '',
     database: 'pentablet',
     port: 3306
 });
