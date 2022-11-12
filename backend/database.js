31eef24b712097f390366d1064b2d42563e60c71
Jannett Arami Aquino Romero <jaquinoromero@alumno.unlam.edu.ar>
2022-11-12 19:55:59 -0300

    conflictos

--- backend/database.js
+++ backend/database.js
@@ -2,11 +2,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-<<<<<<< HEAD
     password: '',
-=======
-    password: '1564',
->>>>>>> 6ec1ed47f0e03c7a50d3eecf303a7ce9ade597b5
     database: 'pentablet',
     port: 3306
 });
